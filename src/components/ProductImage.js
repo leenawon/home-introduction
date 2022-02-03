3a68abd8d441e7dfb2461151273df4e336a0edd0
leenawon <nawon5154@gmail.com>
2022-02-03 22:45:15 +0900

    Add - 페이지 하단의 ProductImage 컴포넌트 작성

---
+++ src/components/ProductImage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'styles/ProductImage.scss';
+
+export default function ProductImage({product, id, setId}) {
+  const imageClick = (productId) => {
+    setId((curr) => curr === productId ? 0 : productId);
+  };
+
+  return (
+    <div className={id === product.productId ? 'product-image point-border' : 'product-image'} key={product.productId} onClick={() => imageClick(product.productId)}>
+      <div style={{backgroundImage: `url(${product.imageUrl})`}}>
+        {!product.outside && 
+          <div className='discount-badge'>
+            {product.discountRate} 
+            <span>%</span>
+          </div>
+        }
+      </div>
+    </div>
+  );
+}
