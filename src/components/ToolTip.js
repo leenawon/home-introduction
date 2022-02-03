df2a931e2b65dd6ec7787785525e2cd3c4bdc294
leenawon <nawon5154@gmail.com>
2022-02-03 22:50:25 +0900

    Add - 돋보기 버튼 및 TooltipInfo 포함하는 Tooltip 컴포넌트 작성

---
+++ src/components/ToolTip.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TooltipInfo from './TooltipInfo';
+import 'styles/Tooltip.scss';
+
+export default function ToolTip({product, id, setId}) {
+  const buttonClick = (productId) => {
+    setId((curr) => curr === productId ? 0 : productId);
+  };
+
+  return (
+    <div className='button-wrapper' style={{top: `${product.pointX*1.6}px`, left: `${product.pointY*1.6+15}px`, display: 'block'}} onClick={() => buttonClick(product.productId)}>
+      {id === product.productId ? 
+        <img src='https://cdn.ggumim.co.kr/storage/20211029145330GwwumnWNSs.png' alt='닫기 버튼' />
+        :
+        <img src='https://cdn.ggumim.co.kr/storage/20211029145238AlZrQ41xtg.png' alt='돋보기 버튼' />
+      }
+      {id === product.productId && (
+        <TooltipInfo product={product} id={id} />
+        )
+      }
+    </div>
+  );
+}
