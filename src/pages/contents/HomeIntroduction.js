d2a172650d5cba010e884f0a9733c848044a3762
leenawon <nawon5154@gmail.com>
2022-02-03 22:54:31 +0900

    Add - API 정보 받아와서 Tooltip, ProductImage 컴포넌트에 data 전달하도록 HomeIntroduction 페이지 작성

---
+++ src/pages/contents/HomeIntroduction.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import useFetch from 'hooks/useFetch';
+import ToolTip from 'components/ToolTip';
+import ProductImage from 'components/ProductImage';
+import 'styles/HomeIntroduction.scss';
+
+export default function HomeIntroduction() {
+  const data = useFetch('https://cdn.ggumim.co.kr/test/image_product_link.json');
+  const productList = data.productList;
+  const [id, setId] = useState(0);
+
+  return (
+    <>
+      {data &&
+        <div className='container'>
+          <h1 className='title'>화이트, 우드, 라탄! 세 가지 컨셉으로 꾸민 내 방</h1>
+          <div className='main-image'>
+            {data.imageUrl && <img src={data.imageUrl} alt='방 이미지' />}
+            {productList && productList.map((product) => (
+              <ToolTip key={product.productId} product={product} id={id} setId={setId} />
+            ))}
+          </div>
+          <div className='product-images-wrapper'>
+            <div className='product-images'>
+              {productList && productList.map((product) => (
+                <ProductImage key={product.productId} product={product} id={id} setId={setId} />
+              ))}
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  );
+}
